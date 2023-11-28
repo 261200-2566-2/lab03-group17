4285822a7588098a594b8147e473adf0803c2133
Delphisss <Kong_led_@hotmail.com>
2023-11-28 19:22:01 +0700

    fix

---
+++ src/Character.java
@@ -0,0 +1,117 @@
+public class Character {
+    private String name;
+    private String gender;
+    private String job;
+    private int level;
+    private int hp;
+    private int mana;
+    private int runSpeed;
+    private int damage; // damage of character when don't have weapon
+    private int shield; // damage of character when don't have weapon
+    private Weapon[] weapon = new Weapon[2];
+    
+    public Character(String name, String gender) {
+        this.name = name;
+        if(gender == "Male" || gender == "male"){
+            this.gender = "Male";
+        }else if(gender == "Female" || gender == "female"){
+            this.gender = "Female";
+        }else{
+            System.out.println("Incorrect gender");
+        }
+        this.job = "Novice";
+        this.level = 1;
+        this.hp = 40;
+        this.mana = 20;
+        this.runSpeed = 34;
+        this.damage = 1;
+        this.shield = 0;
+    }
+
+    // max on lv.99
+    void levelUp() {
+        if(level <= 99){
+            level++;
+            hp = 40 + 5*level;
+            mana = 20 + 5*level;
+            damage = 1 + 5*level;
+            runSpeed = 34 + 2*level;
+            if(runSpeed >= 100){
+                runSpeed = 100;
+            }
+        }
+
+    }
+
+    public void setDamage(){
+        if(weapon[1] != null){
+            damage += weapon[1].getDamage();
+        }
+    }
+    public void setShield(){
+        if(weapon[0] != null){
+            shield += weapon[0].getReduceDamage();
+        }
+    }
+
+    public void setRunSpeed() {
+        int totalWeight = 0;
+    
+        if (weapon[0] != null) {
+            totalWeight += weapon[0].getWeight();
+        }
+    
+        if (weapon[1] != null) {
+            totalWeight += weapon[1].getWeight();
+        }
+    
+        if (totalWeight > 150 && totalWeight <= 200) {
+            runSpeed *= 0.75; // วิ่งช้าลง 25%
+        } else if (totalWeight > 200 && totalWeight <= 250) {
+            runSpeed *= 0.7; // วิ่งช้าลง 30%
+        } else if (totalWeight > 250) {
+            runSpeed *= 0.5; // วิ่งช้าลง 50%
+        }
+    }
+    
+    
+    public void equipWeapon(Weapon weapon1){
+        if(weapon1.getType().equals("shield")){
+            weapon[0] = weapon1;
+            setRunSpeed();
+            setShield();
+        }else if(weapon1.getType().equals("sword")){
+            weapon[1] = weapon1;
+            setRunSpeed();
+            setDamage();
+        }
+    }
+    public void equipWeapon(Weapon weapon1,Weapon weapon2){
+        weapon[0] = weapon1;
+        weapon[1] = weapon2;
+        setRunSpeed();
+        setDamage();
+        setShield();
+    }
+    
+
+    void displayInfo() {
+        System.out.println("=== Character Info ===");
+        System.out.println("Name: " + name);
+        System.out.println("Gender: " + gender);
+        System.out.println("Job: " + job);
+        System.out.println("Level: " + level);
+        System.out.println("HP: " + hp);
+        System.out.println("Mana: " + mana);
+        System.out.println("Run Speed: " + runSpeed);
+        System.out.println("Damage: " + damage);
+        System.out.println("Shield: " + shield);
+        for (Weapon w : weapon) {
+            if (w != null) {
+                System.out.println(w.getType() + ": " + w.getName());
+            }
+        }
+        System.out.println("======================");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,104 +1,62 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // สร้างตัวละคร
-        System.out.println("==== Create Your Character ====");
-        System.out.print("Enter Name: ");
+        System.out.print("Enter character name: ");
         String name = scanner.nextLine();
 
-        String gender = ""; // กำหนดค่าเริ่มต้นเป็น empty string
-        int genderChoice;
-        do {
-            System.out.print("Select Gender (1. Male, 2. Female): ");
-            genderChoice = scanner.nextInt();
-            if (genderChoice == 1) {
-                gender = "Male";
-            } else if (genderChoice == 2) {
-                gender = "Female";
-            } else {
-                System.out.println("Invalid choice. Please choose 1 for Male or 2 for Female.");
-            }
-        } while (genderChoice != 1 && genderChoice != 2);
-
-        String job = "Novice";  // คลาส Novice ที่คุณกำหนด
-
-        CharacterInfo player = new CharacterInfo(name, gender, job);
-        // เลือกอาวุธ
-        System.out.println("==== Choose Your Weapon ====");
-        Weapon[] weapons = {
-                new Weapon("Blade      ", 1, 15, 50),
-                new Weapon("Falchion   ", 2, 20, 80),
-                new Weapon("Sword      ", 3, 75, 100),
-                new Weapon("Byeollungum", 4, 130, 120)
-        };
-
-        int weaponChoice;
-        do {
-            displayEquipmentInfo(weapons, "Weapons");
-            weaponChoice = scanner.nextInt();
-            if (weaponChoice < 1 || weaponChoice > weapons.length) {
-                System.out.println("Invalid choice. Please choose again.");
-            }
-        } while (weaponChoice < 1 || weaponChoice > weapons.length);
-
-        Weapon selectedWeapon = weapons[weaponChoice - 1];
-        System.out.println("===================");
-        System.out.println("You chose: " + selectedWeapon.name);
-        System.out.println("Total Weight: " + selectedWeapon.weight);
-        System.out.println("===================");
-
-        // เลือกโล่
-        System.out.println("Choose Your Shield:");
-        Shield[] shields = {
-                new Shield("Advanced Angelic Guard", 1, 20, 80),
-                new Shield("Angelic Guard         ", 2, 30, 100),
-                new Shield("Thorny Buckler        ", 3, 50, 140),
-                new Shield("OOP Guard             ", 4, 100, 200)
-        };
-
-        int shieldChoice;
-        do {
-            displayEquipmentInfo(shields, "Shields");
-            shieldChoice = scanner.nextInt();
-            if (shieldChoice < 1 || shieldChoice > shields.length) {
-                System.out.println("Invalid choice. Please choose again.");
-            }
-        } while (shieldChoice < 1 || shieldChoice > shields.length);
-
-        Shield selectedShield = shields[shieldChoice - 1];
-        System.out.println("===================");
-        System.out.println("You chose: " + selectedShield.name);
-
-        // คำนวณน้ำหนักและแสดงผล
-        int totalWeight = selectedWeapon.weight + selectedShield.weight;
-        System.out.println("Total Weight: " + totalWeight);
-        System.out.println("===================");
-
-        if (totalWeight > 150 && totalWeight <= 200) {
-            player.runSpeed *= 0.75; // วิ่งช้าลง 25%
-        } else if (totalWeight > 200 && totalWeight <= 250) {
-            player.runSpeed *= 0.7; // วิ่งช้าลง 30%
-        } else if (totalWeight > 250) {
-            player.runSpeed *= 0.5; // วิ่งช้าลง 50%
+        System.out.print("Choose gender (1. Male, 2. Female): ");
+        int genderChoice = scanner.nextInt();
+        String gender;
+        if (genderChoice == 1) {
+            gender = "Male";
+        } else if (genderChoice == 2) {
+            gender = "Female";
+        } else {
+            System.out.println("Incorrect choice. Defaulting to Male.");
+            gender = "Male";
         }
 
-        System.out.println("==== Character Information ====");
+        Character player = new Character(name, gender);
+
         player.displayInfo();
-        System.out.println("==== Equipment ====");
-        System.out.println("Swords: " + selectedWeapon.name);
-        System.out.println("Shields: " + selectedShield.name);
-        System.out.println("===================");
 
-    }
+        System.out.println("\nChoose a starting weapon:");
+        Weapon startingSword = new Weapon("Sword", "Starting Sword", 1, 10, 20);
+        Weapon startingAxe = new Weapon("Axe", "Starting Axe", 1, 15, 25);
+        System.out.println("1. " + startingSword.getName());
+        System.out.println("2. " + startingAxe.getName());
+        System.out.print("Enter weapon choice (1 or 2): ");
+        int weaponChoice = scanner.nextInt();
 
-    private static void displayEquipmentInfo(Object[] equipment, String category) {
-        System.out.println(category + ":");
-        for (int i = 0; i < equipment.length; i++) {
-            System.out.println((i + 1) + ". " + equipment[i].toString());
+        if (weaponChoice == 1) {
+            player.equipWeapon(startingSword);
+        } else if (weaponChoice == 2) {
+            player.equipWeapon(startingAxe);
+        } else {
+            System.out.println("Incorrect choice. Defaulting to Starting Sword.");
+            player.equipWeapon(startingSword);
         }
-        System.out.print("Choose " + category + " (1-" + equipment.length + "): ");
+
+        System.out.println("\nChoose a starting shield:");
+        Weapon woodenShield = new Weapon("Shield", "Wooden Shield", 1, 5, 15);
+        Weapon ironShield = new Weapon("Shield", "Iron Shield", 1, 10, 20);
+        System.out.println("1. " + woodenShield.getName());
+        System.out.println("2. " + ironShield.getName());
+        System.out.print("Enter shield choice (1 or 2): ");
+        int shieldChoice = scanner.nextInt();
+
+        if (shieldChoice == 1) {
+            player.equipWeapon(woodenShield);
+        } else if (shieldChoice == 2) {
+            player.equipWeapon(ironShield);
+        } else {
+            System.out.println("Incorrect choice. Defaulting to Wooden Shield.");
+            player.equipWeapon(woodenShield);
+        }
+        System.out.println(" ");
+        player.displayInfo();
+        scanner.close();
     }
 }
